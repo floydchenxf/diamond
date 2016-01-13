75805b33c2941f93bfa05280245d37678a5ad086
floyd.chenxf <floyd.chenxf@alibaba-inc.com>
2016-01-13 15:56:05 +0800

    优化代码

--- app/src/main/java/com/floyd/diamond/bean/MyImageListener.java
+++ app/src/main/java/com/floyd/diamond/bean/MyImageListener.java
@@ -3,6 +3,7 @@ package com.floyd.diamond.bean;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.view.View;
@@ -10,11 +11,6 @@ import android.widget.ImageView;
 
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
-import com.floyd.diamond.biz.constants.EnvConstants;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 
 /**
  * Created by Administrator on 2015/12/5.
@@ -22,43 +18,21 @@ import java.io.FileOutputStream;
 public class MyImageListener implements ImageLoader.ImageListener {
     private View view;
     private Context context;
-    private String path;
 
     public MyImageListener(View view, Context context) {
         this.view = view;
-        path = EnvConstants.imageRootPath;
+        this.context = context;
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
-        File file = new File(path);
-        if (!file.exists()) {
-            file.mkdir();
-        }
-
-
-        try {
-            Bitmap bitmap = response.getBitmap();
-            if (bitmap != null) {
-                File imgFile = new File(file, MD5Util.encodeBy32BitMD5(response.getRequestUrl()));
-                FileOutputStream fos = new FileOutputStream(imgFile);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
+        Bitmap bitmap = response.getBitmap();
 
         if (view != null && view instanceof ImageView) {
-
-//            Drawable drawable = Drawable.createFromPath(path + "/" + MD5Util.encodeBy32BitMD5(response.getRequestUrl()) + ".jpg");
-            Bitmap bitmap = response.getBitmap();
             ((ImageView) view).setImageBitmap(bitmap);
-
         } else {
-            Drawable drawable = Drawable.createFromPath(path + "/" + MD5Util.encodeBy32BitMD5(response.getRequestUrl()));
+            Drawable drawable = new BitmapDrawable(context.getResources(), bitmap);
             view.setBackground(drawable);
         }
 
--- app/src/main/java/com/floyd/diamond/bean/MyImageLoader.java
+++ app/src/main/java/com/floyd/diamond/bean/MyImageLoader.java
@@ -4,6 +4,9 @@ import android.view.View;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
+import com.floyd.diamond.IMChannel;
+import com.floyd.diamond.IMImageCache;
+import com.floyd.diamond.biz.constants.EnvConstants;
 
 /**
  * Created by Administrator on 2015/11/24.
@@ -15,10 +18,11 @@ public class MyImageLoader extends ImageLoader {
      * @param queue The RequestQueue to use for making image requests.
      */
     public MyImageLoader(RequestQueue queue) {
-        super(queue, MyImageCache.getMyImageCache());
+        super(queue, IMImageCache.findOrCreateCache(
+                IMChannel.getApplication(), EnvConstants.imageRootPath));
     }
 
     public void bindView(String url, View view) {
-        this.get(url, new MyImageListener(view, view.getContext()));
+        this.get(url, new MyImageListener(view, view.getContext().getApplicationContext()));
     }
 }
