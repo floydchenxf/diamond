320f44290e84481796e4b6b7fe1be08e46f27673
floyd.chenxf <floyd.chenxf@alibaba-inc.com>
2016-01-26 00:57:45 +0800

    调整文案

--- app/src/main/java/com/floyd/diamond/biz/constants/APIConstants.java
+++ app/src/main/java/com/floyd/diamond/biz/constants/APIConstants.java
@@ -4,8 +4,8 @@ package com.floyd.diamond.biz.constants;
  * Created by floyd on 15-11-28.
  */
 public class APIConstants {
-//    public static final String HOST = "http://120.27.132.126:9999/mote/";
-    public static final String HOST ="http://139.196.34.98:8888/mote/";
+    public static final String HOST = "http://120.27.132.126:9999/mote/";
+//    public static final String HOST ="http://139.196.34.98:8888/mote/";
     public static final String API_VERIFY_CODE = "api/user/sendVerifyCode";//发送验证码
     public static final String API_CHANGE_PASSWORD_VERIFY_CODE = "api/user/changePasswordByVerifyCode";//修改密码的验证码
     public static final String API_USER_LOGIN = "api/user/login";//登录
